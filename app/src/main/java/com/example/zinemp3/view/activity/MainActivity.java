223f3ddc803e6794ec582dfcd33b2df9f778aa71
GiaBaone <102190252@sv1.dut.udn.vn>
2022-05-29 22:41:31 +0700

    implement main activity

--- app/src/main/java/com/example/zinemp3/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zinemp3/view/activity/MainActivity.java
@@ -1,14 +1,28 @@
-package com.example.zinemp3;
+package com.example.zinemp3.view.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.zinemp3.R;
+import com.example.zinemp3.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
