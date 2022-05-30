e20adf1ce50298984c59bc38067a47800837a55c
viettran <102190252@sv1.dut.udn.vn>
2022-05-31 00:08:23 +0700

    implement model offline

---
+++ app/src/main/java/com/example/zinemp3/model/offline/FavoriteObject.java
@@ -0,0 +1,78 @@
+package com.example.zinemp3.model.offline;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+@Entity(tableName = "FavoriteSongs")
+public class FavoriteObject {
+    @PrimaryKey(autoGenerate = true)
+    private int ID;
+    @ColumnInfo(name = "Tên bài hát")
+    private String nameSong="";
+    @ColumnInfo(name = "Nghệ sĩ")
+    private String nameArtist;
+    @ColumnInfo(name = "Ảnh bài hát")
+    private byte[] imageSong=null;
+    @ColumnInfo(name = "Nhạc")
+    private String linkSong="";
+    @ColumnInfo(name = "ID bài hát")
+    private int id_song;
+
+    public FavoriteObject(String nameSong, String nameArtist, byte[] imageSong, String linkSong, int id_song) {
+        this.nameSong = nameSong;
+        this.nameArtist = nameArtist;
+        this.imageSong = imageSong;
+        this.linkSong = linkSong;
+        this.id_song = id_song;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNameSong() {
+        return nameSong;
+    }
+
+    public void setNameSong(String nameSong) {
+        this.nameSong = nameSong;
+    }
+
+    public String getNameArtist() {
+        return nameArtist;
+    }
+
+    public void setNameArtist(String nameArtist) {
+        this.nameArtist = nameArtist;
+    }
+
+    public byte[] getImageSong() {
+        return imageSong;
+    }
+
+    public void setImageSong(byte[] imageSong) {
+        this.imageSong = imageSong;
+    }
+
+    public String getLinkSong() {
+        return linkSong;
+    }
+
+    public void setLinkSong(String linkSong) {
+        this.linkSong = linkSong;
+    }
+
+    public int getId_song() {
+        return id_song;
+    }
+
+    public void setId_song(int id_song) {
+        this.id_song = id_song;
+    }
+}
