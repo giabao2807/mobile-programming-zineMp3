c9972fe5466608f192f4c9af9a5e34b40a86b892
viettran <102190252@sv1.dut.udn.vn>
2022-05-31 00:12:01 +0700

    implements method handle my artist with sqllite

---
+++ app/src/main/java/com/example/zinemp3/model/offline/MyArtistDatabase.java
@@ -0,0 +1,23 @@
+package com.example.zinemp3.model.offline;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+@Database(entities = {MyArtistObject.class},version = 1)
+@TypeConverters({MyArtistObject.IdSongConverters.class})
+public abstract class MyArtistDatabase extends RoomDatabase {
+    private static final String database_name="artist.db";
+    private static MyArtistDatabase instance;
+    public static synchronized MyArtistDatabase getInstance(Context context){
+        if(instance==null){
+            instance= Room.databaseBuilder(context.getApplicationContext(),MyArtistDatabase.class,database_name)
+                    .allowMainThreadQueries().build();
+        }
+        return instance;
+    }
+    public abstract MyArtistDAO myArtistDAO();
+}
