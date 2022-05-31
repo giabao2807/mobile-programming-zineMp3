a7c79ccb4c6e6c5dc59d4e54e84621d4a4fc662c
ThanhNguyen1392001 <nguyen13701@gmail.com>
2022-06-01 00:38:55 +0700

    add activity LoadingDialog 01.06.2022

---
+++ app/src/main/java/com/example/zinemp3/view/activity/LoadingDialog.java
@@ -0,0 +1,26 @@
+package com.example.zinemp3.view.activity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import com.example.zinemp3.R;
+
+public class LoadingDialog {
+    public static LoadingDialog Instance;
+    public static LoadingDialog getInstance(){
+        if(Instance==null) return Instance=new LoadingDialog();
+        return Instance;
+    }
+    ProgressDialog progressDialog;
+
+    public void StartDialog(Context context){
+        progressDialog=new ProgressDialog(context);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+    }
+
+    public void StopDialog(){
+        progressDialog.dismiss();
+    }
+}
