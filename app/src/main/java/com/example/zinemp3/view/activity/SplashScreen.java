3a70ee678b3ed557a492685f31d54e782ba00637
ThanhNguyen1392001 <nguyen13701@gmail.com>
2022-06-11 23:46:54 +0700

    Implement SplashScreen view 11.06.2022

--- app/src/main/java/com/example/zinemp3/view/activity/SplashScreen.java
+++ app/src/main/java/com/example/zinemp3/view/activity/SplashScreen.java
@@ -1,4 +1,27 @@
 package com.example.zinemp3.view.activity;
 
-public class SplashScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.zinemp3.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+                finish();
+            }
+        },2500);
+    }
 }
